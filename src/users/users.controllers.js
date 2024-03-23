a79b9a74b5c44eaf0030aa4fe759c823fe8868ae
wno <winnyeinoo143@gmail.com>
2024-03-23 20:01:49 +0630

    Fix bug

--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -1,6 +1,6 @@
 import * as services from "./users.services.js";
 
 export const getAll = async (req, res) => {
-  const data = services.getAll();
+  const data = await services.getAll();
   return res.status(200).json({ data: data });
 };
